2e7c4155534e4c5f9d92adfc1f0a4f9d0230e97d
Noemiegrau <noemiegrau@outlook.fr>
2021-03-26 21:44:18 -0700

    added Bulma library

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,17 @@
 // Fullpage.js setup
 new fullpage("#fullpage", {
+
+    // License
+    // licenseKey: 'YOUR_KEY_HERE',
+    
+    // Design
     sectionsColor: ["#ADD8E6", "#ADD8E6", "#ADD8E6"]
+    // responsiveSlides: true
+    // fixedElements: '.hero'
+
+    // Events
+	  // afterResponsive: function(isResponsive){}
+
   });
+
+
