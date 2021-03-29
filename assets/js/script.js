ac80f57e96996dd42087e3a1cbfe5af9b4fa2e71
Kedakai <nicoleteeth@ucla.edu>
2021-03-28 21:34:49 -0700

    add carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,9 @@ new fullpage("#fullpage", {
 
   });
 
+// initialize image URL
+var imgUrl;
+
 // API #1
 function dogApi() {
     var dogFetchUrl = "https://dog.ceo/api/breeds/image/random";
@@ -21,9 +24,11 @@ function dogApi() {
         return response.json();
     })
     .then(function(data) {
-        console.log(data.message);
+        // random dog image URL
+        // console.log(data.message);
+        imgUrl = data.message;
+        console.log("dog fetched");
     });
-
 }
 
 // API #2
@@ -33,22 +38,235 @@ function catApi() {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
     myHeaders.append("x-api-key", catApiKey);
-    
+
     var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow'
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
     };
     
     fetch("https://api.thecatapi.com/v1/images/search?format=json", requestOptions)
     .then(function(response) {
-      return response.json();
+    return response.json();
     })
     .then(function(data) {
-      console.log(data[0].url);
+    imgUrl = data[0].url;
+    console.log("cat fetched");
     });
-
 }
 
-catApi();
-dogApi();
+//Carousel
+class Carousel {
+
+    constructor(element) {
+
+        this.board = element
+
+        // add first two cards programmatically
+        this.push()
+        this.push()
+
+        // handle gestures
+        this.handle()
+
+    }
+
+    handle() {
+
+        // list all cards
+        this.cards = this.board.querySelectorAll('.card')
+
+        // get top card
+        this.topCard = this.cards[this.cards.length - 1]
+
+        // get next card
+        this.nextCard = this.cards[this.cards.length - 2]
+
+        // if at least one card is present
+        if (this.cards.length > 0) {
+
+            // set default top card position and scale
+            this.topCard.style.transform =
+                'translateX(-50%) translateY(-50%) rotate(0deg) rotateY(0deg) scale(1)'
+
+            // destroy previous Hammer instance, if present
+            if (this.hammer) this.hammer.destroy()
+
+            // listen for tap and pan gestures on top card
+            this.hammer = new Hammer(this.topCard)
+            this.hammer.add(new Hammer.Tap())
+            this.hammer.add(new Hammer.Pan({
+                position: Hammer.position_ALL,
+                threshold: 0
+            }))
+
+            // pass events data to custom callbacks
+            this.hammer.on('tap', (e) => {
+                this.onTap(e)
+            })
+            this.hammer.on('pan', (e) => {
+                this.onPan(e)
+            })
+
+        }
+
+    }
+
+    onTap(e) {
+
+        // get finger position on top card
+        let propX = (e.center.x - e.target.getBoundingClientRect().left) / e.target.clientWidth
+
+        // get rotation degrees around Y axis (+/- 15) based on finger position
+        let rotateY = 15 * (propX < 0.05 ? -1 : 1)
+
+        // enable transform transition
+        this.topCard.style.transition = 'transform 100ms ease-out'
+
+        // apply rotation around Y axis
+        this.topCard.style.transform =
+            'translateX(-50%) translateY(-50%) rotate(0deg) rotateY(' + rotateY + 'deg) scale(1)'
+
+        // wait for transition end
+        setTimeout(() => {
+            // reset transform properties
+            this.topCard.style.transform =
+                'translateX(-50%) translateY(-50%) rotate(0deg) rotateY(0deg) scale(1)'
+        }, 100)
+
+    }
+
+    onPan(e) {
+
+        if (!this.isPanning) {
+
+            this.isPanning = true
+
+            // remove transition properties
+            this.topCard.style.transition = null
+            if (this.nextCard) this.nextCard.style.transition = null
+
+            // get top card coordinates in pixels
+            let style = window.getComputedStyle(this.topCard)
+            let mx = style.transform.match(/^matrix\((.+)\)$/)
+            this.startPosX = mx ? parseFloat(mx[1].split(', ')[4]) : 0
+            this.startPosY = mx ? parseFloat(mx[1].split(', ')[5]) : 0
+
+            // get top card bounds
+            let bounds = this.topCard.getBoundingClientRect()
+
+            // get finger position on top card, top (1) or bottom (-1)
+            this.isDraggingFrom =
+                (e.center.y - bounds.top) > this.topCard.clientHeight / 2 ? -1 : 1
+
+        }
+
+        // get new coordinates
+        let posX = e.deltaX + this.startPosX
+        let posY = e.deltaY + this.startPosY
+
+        // get ratio between swiped pixels and the axes
+        let propX = e.deltaX / this.board.clientWidth
+        let propY = e.deltaY / this.board.clientHeight
+
+        // get swipe direction, left (-1) or right (1)
+        let dirX = e.deltaX < 0 ? -1 : 1
+
+        // get degrees of rotation, between 0 and +/- 45
+        let deg = this.isDraggingFrom * dirX * Math.abs(propX) * 45
+
+        // get scale ratio, between .95 and 1
+        let scale = (95 + (5 * Math.abs(propX))) / 100
+
+        // move and rotate top card
+        this.topCard.style.transform =
+            'translateX(' + posX + 'px) translateY(' + posY + 'px) rotate(' + deg + 'deg) rotateY(0deg) scale(1)'
+
+        // scale up next card
+        if (this.nextCard) this.nextCard.style.transform =
+            'translateX(-50%) translateY(-50%) rotate(0deg) rotateY(0deg) scale(' + scale + ')'
+
+        if (e.isFinal) {
+
+            this.isPanning = false
+
+            let successful = false
+
+            // set back transition properties
+            this.topCard.style.transition = 'transform 200ms ease-out'
+            if (this.nextCard) this.nextCard.style.transition = 'transform 100ms linear'
+
+            // check threshold and movement direction
+            if (propX > 0.25 && e.direction == Hammer.DIRECTION_RIGHT) {
+
+                successful = true
+                // get right border position
+                posX = this.board.clientWidth
+
+            } else if (propX < -0.25 && e.direction == Hammer.DIRECTION_LEFT) {
+
+                successful = true
+                // get left border position
+                posX = -(this.board.clientWidth + this.topCard.clientWidth)
+
+            } else if (propY < -0.25 && e.direction == Hammer.DIRECTION_UP) {
+
+                successful = true
+                // get top border position
+                posY = -(this.board.clientHeight + this.topCard.clientHeight)
+
+            }
+
+            if (successful) {
+
+                // throw card in the chosen direction
+                this.topCard.style.transform =
+                    'translateX(' + posX + 'px) translateY(' + posY + 'px) rotate(' + deg + 'deg)'
+
+                // wait transition end
+                setTimeout(() => {
+                    // remove swiped card
+                    this.board.removeChild(this.topCard)
+                    // add new card
+                    this.push()
+                    // handle gestures on new top card
+                    this.handle()
+                }, 200)
+
+            } else {
+
+                // reset cards position and size
+                this.topCard.style.transform =
+                    'translateX(-50%) translateY(-50%) rotate(0deg) rotateY(0deg) scale(1)'
+                if (this.nextCard) this.nextCard.style.transform =
+                    'translateX(-50%) translateY(-50%) rotate(0deg) rotateY(0deg) scale(0.95)'
+
+            }
+
+        }
+
+    }
+
+    push() {
+        let card = document.createElement('div')
+
+        card.classList.add('card')
+
+        if( Math.round(Math.random()) == 0 ) {
+            console.log("Woof");
+            dogApi();
+        } else {
+            console.log("meow");
+            catApi();
+        }
+
+        card.style.backgroundImage =
+            "url('" + imgUrl + "')";
+            
+        this.board.insertBefore(card, this.board.firstChild)
+    }
+}
+
+let board = document.querySelector('#board')
+
+let carousel = new Carousel(board)
