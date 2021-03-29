bbd247b1187f4f049559b61fcf71ee672e7440f2
Kedakai <nicoleteeth@ucla.edu>
2021-03-28 20:32:58 -0700

    revise fetch for cat API

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,10 @@ new fullpage("#fullpage", {
 
     // License
     // licenseKey: 'YOUR_KEY_HERE',
-    
     // Design
     sectionsColor: ["#ADD8E6", "#ADD8E6", "#ADD8E6"]
     // responsiveSlides: true
     // fixedElements: '.hero'
-
     // Events
 	  // afterResponsive: function(isResponsive){}
 
@@ -16,7 +14,7 @@ new fullpage("#fullpage", {
 
 // API #1
 function dogApi() {
-    var dogFetchUrl = "https://dog.ceo/api/breeds/image/random"
+    var dogFetchUrl = "https://dog.ceo/api/breeds/image/random";
 
     fetch(dogFetchUrl)
     .then(function(response) {
@@ -24,13 +22,13 @@ function dogApi() {
     })
     .then(function(data) {
         console.log(data.message);
-        console.log(data.status);
-    })
+    });
+
 }
 
 // API #2
 function catApi() {
-    var catApiKey = "6239a053-498a-49cb-ac5c-e7a746598576"
+    var catApiKey = "6239a053-498a-49cb-ac5c-e7a746598576";
 
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
@@ -43,13 +41,14 @@ function catApi() {
     };
     
     fetch("https://api.thecatapi.com/v1/images/search?format=json", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data[0].url);
+    });
 
 }
-
-
 
 catApi();
 dogApi();
