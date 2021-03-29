1ff031a9e44d080560c44cdff1cf5c115f9b76c0
Kedakai <nicoleteeth@ucla.edu>
2021-03-28 18:07:10 -0700

    add dog and cat APIs

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,4 +14,42 @@ new fullpage("#fullpage", {
 
   });
 
+// API #1
+function dogApi() {
+    var dogFetchUrl = "https://dog.ceo/api/breeds/image/random"
 
+    fetch(dogFetchUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data.message);
+        console.log(data.status);
+    })
+}
+
+// API #2
+function catApi() {
+    var catApiKey = "6239a053-498a-49cb-ac5c-e7a746598576"
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("x-api-key", catApiKey);
+    
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+    
+    fetch("https://api.thecatapi.com/v1/images/search?format=json", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
+}
+
+
+
+catApi();
+dogApi();
