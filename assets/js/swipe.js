585a37ec9fb741385d957676266dd247242e6815
Wyatt Simmons <simmonsw182@gmail.com>
2021-03-31 07:24:28 -0700

    removed unnecessary leftover code blocks from my-profile.js and swipe.js

--- assets/js/swipe.js
+++ assets/js/swipe.js
@@ -73,26 +73,6 @@ new Pageable("#container", {
     },
 });
 
-// var profilesListEl = document.getElementById('profiles-list');
-
-// (function() {
-//     'use strict';
-//     $('.hamburger-menu').click(function (e) {
-//         e.preventDefault();
-//         if ($(this).hasClass('active')){
-//             $(this).removeClass('active');
-//             $('.menu-overlay').fadeToggle( 'fast', 'linear' );
-//             $('.menu .menu-list').slideToggle( 'slow', 'swing' );
-//             $('.hamburger-menu-wrapper').toggleClass('bounce-effect');
-//         } else {
-//             $(this).addClass('active');
-//             $('.menu-overlay').fadeToggle( 'fast', 'linear' );
-//             $('.menu .menu-list').slideToggle( 'slow', 'swing' );
-//             $('.hamburger-menu-wrapper').toggleClass('bounce-effect');
-//         }
-//     })
-//   })();
-
 function dogApi() {
     var dogFetchUrl = "https://dog.ceo/api/breeds/image/random";
     
