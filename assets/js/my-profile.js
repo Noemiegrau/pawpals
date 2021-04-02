452c115bbcd8eee930d7a470717572436ae1e2ca
Noemiegrau <noemiegrau@outlook.fr>
2021-04-02 01:06:45 -0700

    trying to restore develop

--- assets/js/my-profile.js
+++ assets/js/my-profile.js
@@ -1,5 +1,4 @@
 var userProfileObj = {};
-var profilesListEl = document.getElementById('profiles-list');
 
 // Load user profile
 var loadUserData = function() {
@@ -16,3 +15,30 @@ var loadUserData = function() {
 };
 
 loadUserData();
+
+// Function that displays the user profile info into the profile card
+function displayProfileObj () {
+    document.getElementById("animal-name").innerHTML = userProfileObj.name;
+    document.getElementById("animal-age").innerHTML = "Age: " + userProfileObj.age;
+    document.getElementById("animal-type").innerHTML = "Type of animal: " + userProfileObj.animal;
+    document.getElementById("animal-interest").innerHTML = "Interest/Orientation: " + userProfileObj.interest;
+    document.getElementById("animal-image").innerHTML = userProfileObj.profileImg;
+    document.getElementById("min-max").innerHTML = "Min-Max Age: " + userProfileObj.minAge + " - " + userProfileObj.maxAge;
+};
+
+displayProfileObj();
+
+//Add event listener for delete button
+document.getElementById("delete-btn").addEventListener("click", function() {
+    // document.getElementById("animal-image").innerHTML = "You have no profile";
+	// document.getElementById("animal-name").innerHTML = "You have no profile";
+	// document.getElementById("animal-age").innerHTML = "You have no profile";
+	// document.getElementById("animal-type").innerHTML = "You have no profile";
+	// document.getElementById("animal-interest").innerHTML = "You have no profile";
+	// document.getElementById("min-max").innerHTML = "You have no profile";
+	// document.getElementById("my-profile-container").classList.add("hide");
+    document.getElementById("my-profile-container").remove();
+    document.getElementById("analytics-container").remove();
+    storage.removeItem(userProfileObj);
+
+});
